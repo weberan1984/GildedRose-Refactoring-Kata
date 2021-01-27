92d7c853555fc01dd9ebb73375412d0c25cbd16b
weberan1984 <36308266+weberan1984@users.noreply.github.com>
2021-01-27 11:38:18 +0100

    I have implemented the tests for each business case.

--- Java/src/README.md
+++ Java/src/README.md
@@ -1,7 +1,7 @@
 # Andre Weber's solution for the refactoring of Kata example.
 ## Description of the steps done to obtain this solution
 1. I have first adapted the solution to be able to develop in my environment.  I have replaced the usage of junit 5 by junit 4 because I do not yet know the version 5 and I think it doesn't really matter here (it's not the target of this exercise to learn junit 5).
- 
+1.  I have added the business rules into comments in the code and I have added unit-tests to cover all such rules.  
 ## Instructions to compile the solution
 1. If not already done, add the path to the java SDK in the PATH variable
      set path=%path%;C:\Program Files\Java\jdk1.8.0_161\bin
--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,18 +2,39 @@ package com.gildedrose;
 
 
 class GildedRose {
+	
+	public static final String AGED_BRIE = "Aged Brie";
+	public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+	public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     Item[] items;
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
+    /**
+     * Update quality level (integer value between 80 and 0) and the "sel in" (days) value.
+     * This method must be called each day (for example at midnight).
+     * It modifies the attributes "quality" and "sellIn" of the array of items referred in this instance (see attribute "items").
+     * TODO : refactoring of this method
+     * Business rules implemented here (listed in the order of apparition in the specifications we have received):
+     * <ol>
+     *  <li>All items have a SellIn value which denotes the number of days we have to sell the item.  All items have a Quality value which denotes how valuable the item is.  At the end of each day our system lowers both values for every item</li>
+     * 	<li>Once the sell by date has passed, Quality degrades twice as fast</li>
+     * 	<li>The Quality of an item is never negative</li>
+     * 	<li>"Aged Brie" actually increases in Quality the older it gets</li>
+     * 	<li>The Quality of an item is never more than 50</li>
+     * 	<li>"Sulfuras", being a legendary item, never has to be sold or decreases in Quality</li>
+     * 	<li>"Backstage passes", like aged brie, increases in Quality as its SellIn value approaches; Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+	Quality drops to 0 after the concert</li>
+     * </ol>  
+     */
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!items[i].name.equals(AGED_BRIE)
+                    && !items[i].name.equals(BACKSTAGE_PASSES)) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!items[i].name.equals(SULFURAS)) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
@@ -21,7 +42,7 @@ class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (items[i].name.equals(BACKSTAGE_PASSES)) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
@@ -37,15 +58,15 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!items[i].name.equals(SULFURAS)) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!items[i].name.equals(AGED_BRIE)) {
+                    if (!items[i].name.equals(BACKSTAGE_PASSES)) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!items[i].name.equals(SULFURAS)) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
